b4551f3000592c275773402974956b27ca833720
TheDoubleRainbow <16mb.com@gmail.com>
2017-11-18 22:22:28 +0200

    router

--- src/app.vue
+++ src/app.vue
@@ -1,10 +1,18 @@
 require("./lib/vuetify.js")
 require("./vheader.vue")
 require("./mainblock.vue")
+require("./eventpage.vue")
 
+Vue.use(Router)
+const router = new Router({
+	routes: [
+		{ path: '/', component: mainblock}
+	]
+})
 
 app = new Vue({
 	el: "#app",
+	router: router,
 	data: 
 		{
 		},
