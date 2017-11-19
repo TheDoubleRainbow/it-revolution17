554d286bf8fcc61e1fd8b0dffead17e97c6da0ea
Vlad Martyshev <vladmartyshev@MacBook-Pro-Vlad.local>
2017-11-19 11:04:04 +0200

    last comments

--- public/javascripts/bundle.js
+++ public/javascripts/bundle.js
@@ -28390,8 +28390,8 @@ lastcomments = Vue.component('lastcomments', {
 				<div class="sidebar">
 					<div class="sidebartitle">Last Comments</div>
 					<div class="topeventslist">
-						<div @click="openEvent(event)" class="topevent column is-12" v-for="event in list">
-							<div class="topeventname"><img class="lastcommentsimg" src="https://res.cloudinary.com/teepublic/image/private/s--dzAEC90f--/t_Preview/b_rgb:484849,c_limit,f_jpg,h_630,q_90,w_630/v1488911584/production/designs/1298385_1.jpg" />{{event.name}}</div>
+						<div @click="openEvent($event, comment.event_id._id)" class="topevent column is-12" v-for="comment in list">
+							<div class="topeventname"><img class="lastcommentsimg" :src="comment.event_id.peacture" />{{comment.body}}</div>
 						</div>
 					</div>
 				</div>
@@ -28406,18 +28406,20 @@ lastcomments = Vue.component('lastcomments', {
 		this.getTopEvents();
 	},
 	methods: {
-		openEvent: function(event){
-			this.$router.push(`/event/${event.id}`)
+		openEvent: function(event, id){
+			this.$router.push(`/event/${id}`)
 		},
 		getTopEvents: function(){
 			var that = this;
 			that.list = [];
-			axios.get(`/api/topevents`)
+			axios.get(`/api/lastcomments`)
                       .then(function (response){
                       	console.log(response);
-                        response.data.forEach(function(item, i, arr) {
-  							that.list.push({game: item.game, id: item._id, game: item.game, name: item.name, city: item.city, likes: item.rating, date: item.date, peacture: item.peacture, link: item.link})
-                        });
+                        //response.data.forEach(function(item, i, arr) {
+  							//that.list.push({game: item.game, id: item._id, game: item.game, name: item.name, city: item.city, likes: item.rating, date: item.date, peacture: item.peacture, link: item.link})
+  							//that.list.push()
+                        //});
+                        that.list = response.data;
                       })
                       .catch(function (error) {
                         that.list = [];
